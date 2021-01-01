f88e73dd51738419e773e4fd13a6be0a13c20c52
Florentin AP <>
2021-01-01 10:39:52 +0700

    adding homepage and views

--- personal_portfolio-project/portfolio/views.py
+++ personal_portfolio-project/portfolio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import random
+from .models import ProjectFlo
 
 # Create your views here.
+def home(request):
+    projects = ProjectFlo.objects.all()
+    return render(request,'portfolio/home.html',{'projects':projects}) #kok bisa otomatis ambil dr folder templates ya??
