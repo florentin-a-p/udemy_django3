bfb09d4cbed3863229c464fd24af6b7e074395e3
Florentin AP <>
2021-01-04 05:34:25 +0700

    add static resume me and picture

--- personal_portfolio-project/blog/views.py
+++ personal_portfolio-project/blog/views.py
@@ -4,7 +4,8 @@ from .models import ProjectBlogFlo
 
 # Create your views here.
 def all_blogs(request):
-    projects = ProjectBlogFlo.objects.all()
+    # projects = ProjectBlogFlo.objects.all()
+    projects = ProjectBlogFlo.objects.order_by('date')
     return render(request, 'blog/all_blogs.html',{'projects':projects})
 
 
