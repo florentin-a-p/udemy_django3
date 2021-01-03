12635ec6d1b9aa47c539670083845215bc58ea74
Florentin AP <>
2021-01-04 04:58:39 +0700

    make list of blogs

--- personal_portfolio-project/blog/urls.py
+++ personal_portfolio-project/blog/urls.py
@@ -1,7 +1,12 @@
-# from django.urls import path
-# from . import views
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
 
-# urlpatterns = [
-#     path('', views.all_blogs, name='all_blogs'),
-# ]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.all_blogs, name='all_blogs'),
+]
  
+urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
--- personal_portfolio-project/blog/views.py
+++ personal_portfolio-project/blog/views.py
@@ -1,6 +1,10 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import ProjectBlogFlo
 
-# # Create your views here.
+# Create your views here.
+def all_blogs(request):
+    projects = ProjectBlogFlo.objects.all()
+    return render(request, 'blog/all_blogs.html',{'projects':projects})
 
-# def all_blogs(request):
-#     return render(request, 'blog/all_blogs.html')
+
