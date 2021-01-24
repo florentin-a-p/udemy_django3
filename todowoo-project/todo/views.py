6836faef86a2373b3f329263e148096a8d3d41f0
Florentin AP <>
2021-01-24 12:52:37 +0700

    filter todolist by user

--- todowoo-project/todo/views.py
+++ todowoo-project/todo/views.py
@@ -46,22 +46,6 @@ def loginuser(request):
             # login(request, user)
             login(request, user_name)
             return redirect('currenttodos')
-        # # create new user
-        # if request.POST['password1'] == request.POST['password2']:
-        #     try: 
-        #         user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-        #         user.save()
-        #         login(request, user)
-        #         return redirect('currenttodos')
-
-        #     except IntegrityError:
-        #         return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'That username has already been taken. Please choose a new username'})
-        # else:
-        #     #tell the user the passwords didn't match
-        #     # print("hello")
-        #     return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords did not match'})
-
-
 
 def logoutuser(request):
     if request.method == 'POST':
@@ -86,78 +70,7 @@ def createtodo(request):
             return render(request, 'todo/createtodo.html', {'form':TodoForm(), 'error':'bad data passed in. try again'}) 
 
 def currenttodos(request):
-    todos = ProjectTodoWooFlo.objects.all()
+    todos = ProjectTodoWooFlo.objects.filter(user=request.user, memo_complete_date__isnull=True)
     return render(request, 'todo/currenttodos.html',{'todos':todos})
 
 
-
-
-
-
-
-
-
-# def detail_view(request, product_id):
-#     product = Product.objects.get(pk=product_id)
-#     comment_form = CommentForm(request.POST, instance=product)
-#     if comment_form.is_valid():
-#         comment_form.save()
-#         comment_form = CommentForm()
-#     context = {'product': product, 'comment_form': comment_form}
-#     return render(request, 'my_rev/detail_view.html', context)
-
-
-# def detail_view(request, product_id):
-#     product = Product.objects.get(pk=product_id)
-#     if product:
-#         comment_form = CommentForm(request.POST or None, instance=product)
-#         if comment_form.is_valid():
-#             comment_form.save()
-#             comment_form = CommentForm()
-#     context = {'product': product, 'comment_form': comment_form}
-#     return render(request, 'my_rev/detail_view.html', context)
-
-
-
-
-
-
-
-
-
-
-# <body>
-# <h1>{{ product.title }}</h1>
-# <p>
-# <p><strong>PRODUCT DESCRIPTION</strong></p>
-#     {{ product.description }}
-# <p><strong>Price: </strong>Rs.{{ product.price }}</p>
-# <ul>
-# {% for comment in product.comment_set.all %}
-#     <li>{{ comment.comment|capfirst }}</li>
-
-# {% endfor %}
-#         </ul>
-# <p><a href="/comment/"><strong>ADD COMMENT</strong></a></p>
-
-
-
-
-
-# <body>
-# <h1>{{ product.title }}</h1>
-# <p>
-# <p><strong>PRODUCT DESCRIPTION</strong></p>
-#     {{ product.description }}
-# <p><strong>Price: </strong>Rs.{{ product.price }}</p>
-# <ul>
-#    {% for comment in product.comment_set.all %}
-#     <li>{{ comment.comment|capfirst }}</li>
-
-# {% endfor %}
-#         </ul>
-
-# <form method="POST"> {% csrf_token %}
-#     {{ comment_form.as_p }}
-#     <input type="submit" value="Save"/>
-# </form>
